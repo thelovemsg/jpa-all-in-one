cc8137c5e15185385819e610d8300e65cb90e1f2
thelovemsg <thelovemsg@naver.com>
2022-11-12 18:53:03 +0900

    jpa 활용 1,2 스터디 완료!

--- jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop2/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -111,13 +111,13 @@ public class OrderRepository {
     }
 
     public List<Order> findAllWithItem() {
-        return em.createQuery(
-                "select distinct o from Order o" +
-                        " join fetch o.member m" +
-                        " join fetch o.delivery d" +
-                        " join fetch o.orderItems oi" +
-                        " join fetch oi.item i", Order.class)
-                .getResultList();
+        return em.createQuery("" +
+                "select distinct " +
+                "o from Order o " +
+                " join fetch o.member m" +
+                " join fetch o.delivery d"+
+                " join fetch o.orderItems oi"+
+                " join fetch oi.item i", Order.class).getResultList();
     }
 
     public List<Order> findAllWithMemberDelivery(int offset, int limit) {
