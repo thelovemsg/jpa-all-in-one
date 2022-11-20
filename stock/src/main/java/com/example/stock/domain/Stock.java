84987260a95175658e6bc8a065df04c25260f7fe
thelovemsg <thelovemsg@naver.com>
2022-11-20 17:06:36 +0900

    동시성 문제 공부 - 카페

---
+++ stock/src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,33 @@
+package com.example.stock.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Stock {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long productId;
+    private Long quantity;
+    public Stock(){}
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void decrease(Long quantity){
+        if(this.quantity - quantity < 0){
+            throw new RuntimeException();
+        }
+
+        this.quantity = this.quantity - quantity;
+    }
+}
